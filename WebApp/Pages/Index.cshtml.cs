03549964bbf6cec7fd97aff3f85fedf4360628e0
Swati Jadhav <jadhav.swatig@gmail.com>
2022-07-03 00:39:40 +0530

    Add project files.

---
+++ WebApp/Pages/Index.cshtml.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebApp.Model;
+using WebApp.Services;
+
+namespace WebApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public List<Product> products;
+        public void OnGet()
+        {
+            products = new ProductService().GetProducts();
+        }
+    }
+}
---
+++ WebApp/Services/ProductService.cs
@@ -0,0 +1,52 @@
+﻿using System.Data.SqlClient;
+using WebApp.Model;
+
+namespace WebApp.Services
+{
+    public class ProductService
+    {
+        private string SQL_Server = "sqlserver0981.database.windows.net";
+        private string SQL_DB = "sqldb";
+        private string SQL_user = "sqladmin";
+        private string SQL_pwd = "Aditya@1712";
+
+        public SqlConnection GetConnection()
+        {
+            SqlConnectionStringBuilder _builder = new SqlConnectionStringBuilder();
+            _builder.DataSource = SQL_Server;
+            _builder.InitialCatalog = SQL_DB;
+            _builder.UserID = SQL_user;
+            _builder.Password = SQL_pwd;
+
+            return new SqlConnection(_builder.ConnectionString);
+        }
+
+        public List<Product> GetProducts()
+        {
+            SqlConnection conn = GetConnection();
+
+            List<Product> products = new List<Product>();
+
+            SqlCommand command = new SqlCommand("select ProductID,ProductName, Quantity from Products",conn);
+
+            conn.Open();
+            using (SqlDataReader reader = command.ExecuteReader())
+            {
+                while(reader.Read())
+                {
+                    Product product = new Product();
+                    product.ProductID = reader.GetInt32(0);
+                    product.ProductName = reader.GetString(1);
+                    product.Quantity = reader.GetInt32(2);
+                    products.Add(product);
+                }    
+            }
+
+            conn.Close();
+            return products;
+
+        }
+        
+
+    }
+}
